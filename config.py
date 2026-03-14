a05e75858821a69f55b1eee09ab85231942c73e4
rutrutbot <trustpard@gmail.com>
2026-03-14 22:16:24 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+ADMIN_IDS = [int(id) for id in os.getenv('ADMIN_IDS', '').split(',') if id]
+PROXY_URL = os.getenv('PROXY_URL')
+
+# Начальный номер заявки
+INITIAL_ORDER_NUMBER = 10207903
+
+# Время на оплату (в секундах)
+PAYMENT_TIMEOUT = 30 * 60  # 30 минут
